f487350dfbf9233b691e4b1a63d478bfdf9a1e1a
anamariaow <new_annemarie@yahoo.com>
2023-11-23 22:20:22 +0100

    Aggiunto Classe Triangolo

---
+++ src/Forma.java
@@ -0,0 +1,28 @@
+public class Forma {
+    private double lunghezza;
+    private double larghezza;
+
+    public Forma(double lunghezza, double larghezza) {
+        this.lunghezza = lunghezza;
+        this.larghezza = larghezza;
+    }
+
+    public void calcolaArea() {
+    };
+
+    public double getLarghezza() {
+        return larghezza;
+    }
+
+    public void setLarghezza(double larghezza) {
+        this.larghezza = larghezza;
+    }
+
+    public double getLunghezza() {
+        return lunghezza;
+    }
+
+    public void setLunghezza(double lunghezza) {
+        this.lunghezza = lunghezza;
+    }
+}
